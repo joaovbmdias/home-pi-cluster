f2458b972041a37ada0ed91880dfecdc8246af19
Joao Dias <joaovbmdias@icloud.com>
2020-04-29 17:58:50 +0100

    Initial Setup
    Fixed main call to initial setup and second draft of initial_setup

--- initial-pi-setup.sh
+++ initial-pi-setup.sh
@@ -1,19 +1,45 @@
-ECHO update system
-sudo apt-get update -y && sudo apt-get upgrade -y
-ECHO successfully updated 
 
-ECHO change user password
-passwd
+#!/bin/bash
 
-ECHO change hostname
-sudo vi /etc/hostname
+new_password=raspberry
+user=pi
 
-ECHO configure static IP
-sudo vi /etc/dhcpcd.conf
+new_hostname=raspberrypi
+cur_hostname="$(hostname)"
 
-ECHO enable container features
-sudo vi /boot/cmdline.txt #cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory
+echo
+echo System Update
+#sudo apt-get update -y && sudo apt-get upgrade -y
+echo Successfully Updated
+echo
 
-ECHO switch Debian firewall to legacy config
-sudo update-alternatives --set iptables /usr/sbin/iptables-legacy
-sudo update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
+echo
+echo Change User Password
+echo "$user"':'"$new_password" | sudo chpasswd
+echo
+
+echo
+echo Changing Hostname from "$cur_hostname" to "$new_hostname"
+hostnamectl set-hostname $new_hostname
+echo Successfully Changed Hostname
+echo
+
+echo
+echo Configure Static IP
+echo "static ip_address=192.168.1.40/24" >> /etc/dhcpcd.conf
+echo "static routers=192.168.1.1" >> /etc/dhcpcd.conf
+echo "static domain_name_servers=192.168.1.204" >> /etc/dhcpcd.conf
+echo
+
+echo
+echo Enable Container Features
+sed -i ' 1 s/.*/& cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory/' /boot/cmdline.txt
+echo Enabled
+echo
+
+echo
+echo Switch Debian Firewall to Legacy Configurations
+update-alternatives --set iptables /usr/sbin/iptables-legacy
+update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
+echo Done
+echo
--- main.sh
+++ main.sh
@@ -1,7 +1,12 @@
+#!/bin/bash
+#cur_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 initial_setup=true
 deploy_server=true
 external_storage_mount=true
 
+echo Executing Initial Setup
 if [ "$initial_setup" = true ] ; then
-    ./initial_setup.sh
+    chmod +x initial-pi-setup.sh
+    sh initial-pi-setup.sh
 fi
